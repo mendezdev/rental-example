bf4379ba6695dda8ee5a4cfb70670cac314ccb56
Pablo Mendez <mendez.developer@gmail.com>
2018-12-14 11:20:28 -0300

    added rental descsriptions constant and enum type

---
+++ IntiveFDV/Models/Constants/RentalDescriptionConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Constants
+{
+    public class RentalDescriptionConstant
+    {
+        public readonly string HOUR = "By Hour";
+        public readonly string DAY = "By Day";
+        public readonly string WEEK = "By Week";
+        public readonly string FAMILY_PROMOTION = "Family Promotion";
+    }
+}
