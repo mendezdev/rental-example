534a077a0b97609a399585974e716d106445cb9b
Pablo Mendez <mendez.developer@gmail.com>
2018-12-16 11:41:25 -0300

    added FluentValidation, added customer and rental field validators

---
+++ IntiveFDV/Domain.Implementation/Validators/RentalFieldValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Models.Requests;
+
+namespace Domain.Implementation.Validators
+{
+    public class RentalFieldValidator : AbstractValidator<RentalRequest>
+    {
+        private readonly string baseErrorMessage = "Need to specify";
+
+        public RentalFieldValidator()
+        {
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage($"{baseErrorMessage} a quantity");
+            RuleFor(x => x.RentalType)
+                .IsInEnum().WithMessage($"{baseErrorMessage} an option rent");
+            RuleFor(x => x.Customer).SetValidator(new CustomerValidator());
+        }
+    }
+}
